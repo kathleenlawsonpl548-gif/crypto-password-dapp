89e6c65a49bb633a53a32c9bb3a8a646886197e9
kathleenlawsonpl548-gif <kathleenlawsonpl548@gmail.com>
2026-03-30 14:17:37 +0100

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,1 +1,12 @@
+import ConnectWallet from './components/ConnectWallet';
 
+function App() {
+  return (
+    <div className="min-h-screen bg-zinc-950 text-white">
+      <div className="max-w-4xl mx-auto pt-20 px-6">
+        <ConnectWallet />
+        {/* Password Generator and Vault components will go here later */}
+      </div>
+    </div>
+  );
+}
